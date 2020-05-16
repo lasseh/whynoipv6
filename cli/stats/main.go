d4cfc99f054e90b55714568cea221749bdce67ac
Lasse Haugen <haugen.lasse@gmail.com>
2020-05-16 16:24:24 +0200

    fixed sql bug and added bgp info

--- cli/stats/main.go
+++ cli/stats/main.go
@@ -4,8 +4,12 @@ package main
 // This should run every night
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"math"
+	"net"
+	"net/http"
 	"time"
 
 	"github.com/gobuffalo/envy"
@@ -49,6 +53,43 @@ type Asn struct {
 	PercentV6 float64
 }
 
+type bgpInfo struct {
+	Status        string `json:"status"`
+	StatusMessage string `json:"status_message"`
+	Data          struct {
+		Asn               int         `json:"asn"`
+		Name              string      `json:"name"`
+		DescriptionShort  string      `json:"description_short"`
+		DescriptionFull   []string    `json:"description_full"`
+		CountryCode       string      `json:"country_code"`
+		Website           interface{} `json:"website"`
+		EmailContacts     []string    `json:"email_contacts"`
+		AbuseContacts     []string    `json:"abuse_contacts"`
+		LookingGlass      interface{} `json:"looking_glass"`
+		TrafficEstimation interface{} `json:"traffic_estimation"`
+		TrafficRatio      interface{} `json:"traffic_ratio"`
+		OwnerAddress      []string    `json:"owner_address"`
+		RirAllocation     struct {
+			RirName          string `json:"rir_name"`
+			CountryCode      string `json:"country_code"`
+			DateAllocated    string `json:"date_allocated"`
+			AllocationStatus string `json:"allocation_status"`
+		} `json:"rir_allocation"`
+		IanaAssignment struct {
+			AssignmentStatus string      `json:"assignment_status"`
+			Description      string      `json:"description"`
+			WhoisServer      string      `json:"whois_server"`
+			DateAssigned     interface{} `json:"date_assigned"`
+		} `json:"iana_assignment"`
+		DateUpdated string `json:"date_updated"`
+	} `json:"data"`
+	Meta struct {
+		TimeZone      string `json:"time_zone"`
+		APIVersion    int    `json:"api_version"`
+		ExecutionTime string `json:"execution_time"`
+	} `json:"@meta"`
+}
+
 func main() {
 	// Load .env file
 	envy.Load("../../.env", "$GOROOT/src/github.com/lasseh/whynoipv6/.env")
@@ -66,7 +107,7 @@ func main() {
 		fmt.Println("Error connecting to database:", err)
 	}
 	defer db.Close()
-	db.LogMode(true)
+	// db.LogMode(true)
 
 	// v6 stats for all sites
 	// this will be used to generate graphs for the progression of v6 deployment
@@ -86,7 +127,7 @@ func main() {
 	// Push to database
 	// This creates stats for the total of all sites
 	// run this every day so we can generate graphs from the data
-	db.Create(&s)
+	db.Table("stats").Create(&s)
 
 	//
 	// Generate stats per ASN
@@ -109,20 +150,57 @@ func main() {
 		v.PercentV6 = PercentOf(v.CountV6, sum)
 
 		// Get ASN Name
-		// NotImplemented
+		v.Asname = asName(v.Asn)
+
+		// Update
+		r := db.Exec("UPDATE asn SET count_v4 = ?, count_v6 = ?, percent_v4 = ?, percent_v6 = ?, asname = ? WHERE asn = ?", v.CountV4, v.CountV6, v.PercentV4, v.PercentV6, v.Asname, v.Asn)
+		// Insert row if not exists
+		if r.RowsAffected == 0 {
+			db.Table("asn").Save(&v)
+		}
 
 		// Print
 		fmt.Printf("ASN: %v CountV4: %v CountV6: %v\n", v.Asn, v.CountV4, v.CountV6)
-
-		// Create
-		db.Table("asn").Create(v)
-		//db.Table("asn").
 	}
-
 }
 
 // PercentOf calculate [number1] is what percent of [number2]
 func PercentOf(current int, all int) float64 {
 	percent := (float64(current) * float64(100)) / float64(all)
 	return percent
+}
+
+func asName(asn int) string {
+	// HTTP Client
+	var netTransport = &http.Transport{
+		Dial: (&net.Dialer{
+			Timeout: 5 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 5 * time.Second,
+	}
+	client := &http.Client{
+		Timeout:   time.Second * 10,
+		Transport: netTransport,
+	}
+	req, _ := http.NewRequest("GET", fmt.Sprintf("https://api.bgpview.io/asn/%d", asn), nil)
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println("Errored when sending request to the server")
+	}
+
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	var info bgpInfo
+	err = json.Unmarshal(body, &info)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Println("AS Name:", info.Data.DescriptionShort)
+
+	// Sleep so we dont get banned from bgpview.io
+	time.Sleep(1 * time.Second)
+
+	return info.Data.DescriptionShort
 }
