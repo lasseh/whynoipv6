ec114dfc139f8efb21f7f5e722442d62f1b7c137
Lasse Haugen <haugen.lasse@gmail.com>
2024-01-14 15:46:38 +0100

    Add logging and error handling to HealthCheckUpdate function

--- internal/toolbox/healthcheck.go
+++ internal/toolbox/healthcheck.go
@@ -1,16 +1,19 @@
 package toolbox
 
 import (
+	"errors"
 	"fmt"
-	"log"
 	"net/http"
 	"time"
+	"whynoipv6/internal/logger"
 )
 
 // HealthCheckUpdate sends a successful health check notification to BetterUptime.com.
 // The function takes a unique identifier (uuid) as input and sends an HTTP HEAD request to BetterUptime.com's API.
 // If there's an error, it will log the error message.
 func (s *Service) HealthCheckUpdate(uuid string) {
+	var log = logger.GetLogger()
+	log = log.With().Str("service", "HealthCheckUpdate").Logger()
 	// Create an HTTP client with a 10-second timeout.
 	httpClient := &http.Client{
 		Timeout: 10 * time.Second,
@@ -24,7 +27,7 @@ func (s *Service) HealthCheckUpdate(uuid string) {
 
 	// If there's an error, log the error message.
 	if err != nil {
-		log.Printf("Error while sending health check update: %s\n", err)
+		log.Err(err).Msg("Error while sending health check update.")
 		return
 	}
 
@@ -33,8 +36,9 @@ func (s *Service) HealthCheckUpdate(uuid string) {
 
 	// Check if the response status code indicates success (2xx).
 	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
-		log.Println("Successfully sent health check update.")
+		log.Debug().Msg("Successfully sent healthcheck update.")
 	} else {
-		log.Printf("Failed to send health check update. Status code: %d\n", resp.StatusCode)
+		// log.Printf("Failed to send health check update. Status code: %d\n", resp.StatusCode)
+		log.Err(errors.New("Status Code:" + resp.Status)).Msg("Failed to send health check update.")
 	}
 }
