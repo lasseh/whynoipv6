acd699f328c11857e461db3e12d3967c49140139
Lasse Haugen <haugen.lasse@gmail.com>
2024-02-21 08:51:58 +0100

    Add dump command to dump all domains per country to a file

---
+++ cmd/v6manage/cmd/dump.go
@@ -0,0 +1,62 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+	"whynoipv6/internal/core"
+
+	"github.com/spf13/cobra"
+)
+
+// crawlCmd represents the crawl command
+var dumpCmd = &cobra.Command{
+	Use:   "dump",
+	Short: "Dumps all domains per country to a file",
+	Long:  "Dump all domains per country to a file",
+	Run: func(cmd *cobra.Command, args []string) {
+		dumpData()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(dumpCmd)
+}
+
+func dumpData() {
+	ctx := context.Background()
+
+	// Spawn core service
+	// domainService := core.NewDomainService(db)
+	countryService := core.NewCountryService(db)
+
+	// start time
+	t := time.Now()
+	log.Println("Dumping country data...")
+
+	// Get all countries
+	country, err := countryService.List(ctx)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// Loop through countries
+	for _, c := range country {
+		fmt.Println("Country: ", c.Country)
+
+		// Get all domains for country
+		// domains, err := countryService.GetCountryCode()(ctx, c.Country)
+		// if err != nil {
+		// 	log.Fatal(err.Error())
+		// }
+
+		// // Loop through domains
+		// for _, d := range domains {
+		// 	fmt.Println(d.Domain)
+		// }
+
+	}
+	// Print time it took to import
+	fmt.Printf("Dumped country data in: %s", time.Since(t))
+}
--- cmd/v6manage/cmd/import.go
+++ cmd/v6manage/cmd/import.go
@@ -35,7 +35,7 @@ func importData() {
 	log.Println("Importing data...")
 
 	var offset int64 = 0
-	var limit int64 = 10000
+	var limit int64 = 1000
 	// Main loop
 	for {
 		sites, err := siteService.ListSite(ctx, offset, limit)
@@ -70,57 +70,3 @@ func importData() {
 		log.Println("Failed to space out domain timestamps: ", err)
 	}
 }
-
-// func importDataWG() {
-// 	ctx := context.Background()
-
-// 	// Spawn core service
-// 	siteService := core.NewSiteService(db)
-// 	domainService := core.NewDomainService(db)
-
-// 	// start time
-// 	t := time.Now()
-// 	log.Println("Importing data...")
-
-// 	var offset int32 = 0
-// 	var limit int32 = 10000
-// 	// Main loop
-// 	for {
-// 		sites, err := siteService.ListSite(ctx, offset, limit)
-// 		if err != nil {
-// 			log.Fatal(err.Error())
-// 		}
-
-// 		// Stop if no more data
-// 		if len(sites) == 0 {
-// 			log.Println("No sites left to import")
-// 			break
-// 		}
-
-// 		// Use wait group to synchronize goroutines
-// 		var wg sync.WaitGroup
-// 		wg.Add(len(sites))
-
-// 		// Loop through sites
-// 		for _, s := range sites {
-// 			// Process sites concurrently
-// 			go func(site core.SiteModel) {
-// 				defer wg.Done()
-
-// 				err := domainService.InsertDomain(ctx, site.Site)
-// 				if err != nil {
-// 					log.Println(err)
-// 				}
-// 			}(s)
-// 		}
-
-// 		// Wait for all goroutines to finish
-// 		wg.Wait()
-
-// 		// Increment offset
-// 		offset += limit
-// 	}
-
-// 	// Print time it took to import
-// 	log.Println("Imported sites in", time.Since(t))
-// }
