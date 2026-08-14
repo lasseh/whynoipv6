404ba98242feac7348a4752f369aa128b1701153
Lasse <haugen.lasse@gmail.com>
2026-08-14 21:05:04 +0200

    fix(frontend): grow the banner close button to a 32px touch target

--- frontend/src/components/Notification.vue
+++ frontend/src/components/Notification.vue
@@ -59,8 +59,12 @@ onUnmounted(() => {
               <div>You're reading an IPv6 shame site over IPv4. Ask your ISP why.</div>
             </div>
           </div>
-          <button class="opacity-70 hover:opacity-80 ml-3 mt-[3px]" @click="hideNotification">
-            <div class="sr-only">Close</div>
+          <button
+            type="button"
+            class="opacity-70 hover:opacity-80 ml-1 p-2 -m-1"
+            @click="hideNotification"
+          >
+            <span class="sr-only">Close</span>
             <svg class="w-4 h-4 fill-current">
               <path
                 d="M7.95 6.536l4.242-4.243a1 1 0 111.415 1.414L9.364 7.95l4.243 4.242a1 1 0 11-1.415 1.415L7.95 9.364l-4.243 4.243a1 1 0 01-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 011.414-1.414L7.95 6.536z"
